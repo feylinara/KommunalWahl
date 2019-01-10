d0536fb11c156e8e36939fc1e493878e46bbb33e
Fey Naomi Schrewe <fey@posteo.eu>
2019-01-10 21:28:57 +0100

    Implenment social behaviour

--- src/kommunalwahl/Voter.java
+++ src/kommunalwahl/Voter.java
@@ -7,10 +7,11 @@ import repast.simphony.engine.schedule.ScheduledMethod;
 
 public class Voter {
 	private HashMap<Party, Double> opinion;
+	private HashMap<Party, Double> _opinion;
 	private double naivite;
 
 	Voter(HashMap<Party, Double> opinion, double naivite) {
-		this.opinion = opinion;
+		this.opinion = this._opinion = opinion;
 		this.naivite = naivite;
 	}
 
@@ -20,17 +21,12 @@ public class Voter {
 	 * @param party
 	 */
 	void increaseInfluence(Party party, double influence) {
-		double opinion = this.opinion.get(party);
+		double opinion = this._opinion.get(party);
 		opinion += naivite * influence;
-		this.opinion.put(party, opinion);
+		this._opinion.put(party, opinion);
 	}
 
-	/**
-	 * The id of the Party the Voter has the highest opinion of
-	 * 
-	 * @return
-	 */
-	public int wouldVote() {
+        Party _wouldVote() {
 		double max = Double.NEGATIVE_INFINITY;
 		Party maxParty = null;
 		for (Entry<Party, Double> entry : opinion.entrySet()) {
@@ -40,15 +36,35 @@ public class Voter {
 				maxParty = entry.getKey();
 			}
 		}
-		return maxParty.id;
+		return maxParty;
+        }
+
+	/**
+	 * The id of the Party the Voter has the highest opinion of
+	 * 
+	 * @return
+	 */
+	public int wouldVote() {
+            return _wouldVote().id;
 	}
 
 	@ScheduledMethod(start = 1.0, interval = 1.0)
 	public void step() {
-		// _opinion = (HashMap<Party, Double>) opinion.clone();
+		Context<Object> context = (Context<Object>) ContextUtils.getContext(this);
+		Network<Object> network = (Network<Object>) context.getProjection("social_network");
+                Party party = _wouldVote();
+
+		for (RepastEdge<Object> edge : network.getOutEdges(this)) {
+			Object obj = edge.getTarget();
+			if (obj instanceof Voter) {
+				((Voter) obj).increaseInfluence(party, influence);
+			}
+		}
 	}
 
 	@ScheduledMethod(start = 0.5, interval = 1.0)
 	public void update() {
-	}
+                opinion = _opinion;
+                _opinion = (HashMap<Party, Double>) opinion.clone();
+        }
 }
